4315b9516b9df52bf6dafa58d277c9f7ce22376a
maurocoimbra <maurocoimbrab@gmail.com>
2024-09-06 11:55:05 -0300

    LinkHover component added

--- frontend/src/components/Shared/NavBar.tsx
+++ frontend/src/components/Shared/NavBar.tsx
@@ -3,10 +3,11 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
+
 import { SearchBar1 } from "@/components/ui/SearchBar1"
+import LinkHover from "@/components/Shared/LinkHover"
 
 import { ModeToggle } from "./mode-toggle";
-import { IMAGES_MANIFEST } from "next/dist/shared/lib/constants";
 
 export default function NavBar() {
 
@@ -17,42 +18,11 @@ export default function NavBar() {
       >
         <div className="container mx-auto flex items-center justify-between h-full">
           <div className="flex space-x-4 gap-5 justify-center items-center">
-            <Link
-              href="/blog"
-              className="relative text-sm dark:text-dark-text hover:text-blue-500 group transition duration-300"
-            >
-              BLOG
-              {/* <span className="block max-w-0 group-hover:max-w-full transition-all duration-300 h-0.5 bg-blue-500"></span> */}
-              <span className="block absolute left-1/2 transform -translate-x-1/2 w-0 group-hover:w-full transition-all duration-300 h-0.5 bg-blue-500 dark:bg-white dark:bg-white"></span>
-            </Link>
-            <Link
-              href="/usuarios"
-              className="relative text-sm dark:text-dark-text hover:text-blue-500 group transition duration-300"
-            >
-              USUÁRIOS
-              <span className="block absolute left-1/2 transform -translate-x-1/2 w-0 group-hover:w-full transition-all duration-300 h-0.5 bg-blue-500 dark:bg-white"></span>
-            </Link>
-            <Link
-              href="/projetos"
-              className="relative text-sm dark:text-dark-text hover:text-blue-500 group transition duration-300"
-            >
-              PROJETOS
-              <span className="block absolute left-1/2 transform -translate-x-1/2 w-0 group-hover:w-full transition-all duration-300 h-0.5 bg-blue-500 dark:bg-white"></span>
-            </Link>
-            <Link
-              href="/tadea"
-              className="relative text-sm dark:text-dark-text hover:text-blue-500 group transition duration-300"
-            >
-              TADEA
-              <span className="block absolute left-1/2 transform -translate-x-1/2 w-0 group-hover:w-full transition-all duration-300 h-0.5 bg-blue-500 dark:bg-white"></span>
-            </Link>
-            <Link
-              href="/faq"
-              className="relative text-sm dark:text-dark-text hover:text-blue-500 group transition duration-300"
-            >
-              FAQ
-              <span className="block absolute left-1/2 transform -translate-x-1/2 w-0 group-hover:w-full transition-all duration-300 h-0.5 bg-blue-500 dark:bg-white"></span>
-            </Link>
+            <LinkHover href="/blog">BLOG</LinkHover>
+            <LinkHover href="/usuarios">USUÁRIOS</LinkHover>
+            <LinkHover href="/projetos">PROJETOS</LinkHover>
+            <LinkHover href="/tadea">TADEA</LinkHover>
+            <LinkHover href="/faq">FAQ</LinkHover>
           </div>
 
           <div className="flex space-x-4 gap-5 justify-center items-center">
@@ -61,20 +31,8 @@ export default function NavBar() {
 
           <div className="flex space-x-4 gap-5 justify-center items-center">
             <SearchBar1 type="search" placeholder="Pesquisar"/>
-            <Link
-              href="/sobre"
-              className="relative text-sm dark:text-dark-text hover:text-blue-500 group transition duration-300"
-            >
-              SOBRE
-              <span className="block absolute left-1/2 transform -translate-x-1/2 w-0 group-hover:w-full transition-all duration-300 h-0.5 bg-blue-500 dark:bg-white"></span>
-            </Link>
-            <Link
-              href="/login"
-              className="relative text-sm dark:text-dark-text hover:text-blue-500 group transition duration-300"
-            >
-              LOGIN
-              <span className="block absolute left-1/2 transform -translate-x-1/2 w-0 group-hover:w-full transition-all duration-300 h-0.5 bg-blue-500 dark:bg-white"></span>
-            </Link>
+            <LinkHover href="/sobre">SOBRE</LinkHover>
+            <LinkHover href="/login">LOGIN</LinkHover>
             <ModeToggle/>
           </div>
         </div>
