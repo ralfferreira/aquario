4cfa42a667c85a0af52c36a94f2987546f6cb909
Ian <ianbittencourt03@gmail.com>
2024-09-12 17:06:09 -0300

    feat add OverlappingImages and PostCard components

---
+++ frontend/src/components/Shared/OverlappingImages.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+interface User {
+  name: string;
+  image: string;
+}
+
+interface OverlappingImagesProps {
+  users: User[];
+}
+
+const OverlappingImages: React.FC<OverlappingImagesProps> = ({ users }) => {
+    const limitedUsers = users.slice(0, 3);
+
+  return (
+    <div className="flex -space-x-6">
+      {limitedUsers.map((user, index) => (
+        <img
+          key={index}
+          src={user.image}
+          alt={user.name}
+          className="w-9 h-9 rounded-full"
+          style={{ zIndex: limitedUsers.length - index }}
+        />
+      ))}
+
+        {users.length > 3 && (
+            <div className="w-9 h-9 flex items-center justify-center text-black pl-9" style={{ zIndex: 0 }}>
+            +{users.length - 3}
+            </div>
+        )}
+    </div>
+  );
+};
+
+export default OverlappingImages;
