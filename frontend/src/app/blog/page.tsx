8e1da96bc2d6258b666e8840558842402f3cf5af
maurocoimbra <maurocoimbrab@gmail.com>
2024-09-12 10:47:21 -0300

    hamburgermenu

--- frontend/src/app/blog/page.tsx
+++ frontend/src/app/blog/page.tsx
@@ -1,8 +1,18 @@
+import HamburgerMenu from "@/components/Shared/HamburgerMenu";
+
 export default function Blog() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <p>Blog Page</p>
+        <HamburgerMenu></HamburgerMenu>
       </main>
     );
   }
+
+// export default function Blog() {
+//     return (
+//       <main className="flex min-h-screen flex-col items-center justify-between p-24">
+//         <p>Blog Page</p>
+//       </main>
+//     );
+//   }
   
