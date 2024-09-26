4a298ec8a5f711400a728da8fa73a85bfe9f1de3
maurocoimbra <maurocoimbrab@gmail.com>
2024-09-26 11:14:34 -0300

    blogfix

--- frontend/src/app/blog/page.tsx
+++ frontend/src/app/blog/page.tsx
@@ -1,6 +1,3 @@
-import HamburgerMenu from "@/components/Shared/HamburgerMenu";
-import NavWrapper from "@/components/Shared/NavWrapper";
-
 export default function Blog() {
     return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
