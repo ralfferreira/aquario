b4e74aa1c6cfca126bfd0acb716d798394644596
Matheus Ferreira <matheusfer0902@gmail.com>
2024-11-11 18:40:02 -0300

    fix search in navbar dark mode

--- frontend/src/components/ui/searchbar1.tsx
+++ frontend/src/components/ui/searchbar1.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-
 import { cn } from "@/lib/utils"
 import { Search } from 'lucide-react';
 
@@ -10,11 +9,12 @@ const SearchBar1 = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
       <div className="relative w-full">
-        <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground w-5" />
+        <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-muted-foreground w-5 dark:text-zinc-300" />
         <input
           type={type}
           className={cn(
             "flex h-10 w-full rounded-full border border-input bg-background pl-10 pr-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+            "dark:border-zinc-300 dark:text-zinc-300 dark:placeholder:text-zinc-300",
             className
           )}
           ref={ref}
